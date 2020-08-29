4199f3fe80209842e9e923f678cb61d901ef3531
Pinkcolorrr <Pinkcolorrrs@gmail.com>
2020-08-30 01:46:00 +0500

    init commit

---
+++ js/script.js
@@ -0,0 +1,101 @@
+(function () {
+    let tasks = {
+        current: [{
+                taskId: doId(),
+                taskContent: "Таsk 1",
+                taskState: "current"
+            },
+            {
+                taskId: doId(),
+                taskContent: "Таsk 2",
+                taskState: "current"
+            }
+        ],
+        done: [],
+        get allTask() {
+            return this.current.length + this.done.length;
+        },
+    };
+
+    const buttonAdd = document.getElementById('button-add');
+    const todoInput = document.getElementById('todo__input');
+    const todoList = document.getElementById('todo__list');
+    const sectionCurrent = document.getElementById('todo__sections-current');
+    const sectionDone = document.getElementById('todo__sections-done');
+    const sectionTrash = document.getElementById('todo__sections-trash');
+
+    function INIT() {
+        for (const item of tasks.current) {
+            createItem(item);
+        }
+    }
+
+    function createItem(el) {
+        let item = document.createElement('li');
+        let text = document.createElement('span');
+        let itemButtons = document.createElement('span');
+        let buttonRemove = document.createElement('button');
+        let buttonDone = document.createElement('button');
+
+        item.classList.add("todo__item");
+        text.classList.add("todo__item-text");
+        itemButtons.classList.add("todo__buttons");
+        buttonRemove.classList.add("button-remove");
+        buttonDone.classList.add("button-done");
+
+        text.innerHTML = el.taskContent;
+
+        buttonRemove.addEventListener('click', (e) => {
+            removeTask(e.target);
+        });
+
+        buttonDone.addEventListener('click', () => {});
+
+        item.id = el.taskId;
+        item.appendChild(text);
+        item.appendChild(itemButtons);
+        itemButtons.appendChild(buttonDone);
+        itemButtons.appendChild(buttonRemove);
+        todoList.appendChild(item);
+    }
+
+    function removeTask(elem) {
+        let removeElem = elem.parentNode.parentNode;
+        let removeId = removeElem.id;
+
+        for (let i = 0; i < tasks.current.length; i++) {
+            if (tasks.current[i].taskId === removeId) {
+                tasks.current.splice(i, 1)
+            }
+        };
+
+        removeElem.remove();
+    }
+
+    function addTask(str) {
+        let elem = {
+            taskId: doId(),
+            taskContent: str,
+            taskState: "current"
+        }
+        tasks.current.push(elem);
+        createItem(elem);
+    }
+
+    function doId() {
+        return Math.random().toString(36).substr(2, 16);
+    }
+
+    buttonAdd.addEventListener('click', () => {
+        if (todoInput.value != "") {
+            addTask(todoInput.value);
+            todoInput.value = "";
+        }
+    });
+
+    sectionCurrent.addEventListener('click', () => {
+        console.log('hi');
+    });
+
+    INIT();
+})();
