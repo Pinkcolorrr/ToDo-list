b1c8be9dd63c2cba07217d68fc3920ba75a5de20
Pinkcolorrr <Pinkcolorrrs@gmail.com>
2020-09-16 16:15:19 +0500

    confirmEl bug fixed

--- js/custom-confirm.js
+++ js/custom-confirm.js
@@ -49,7 +49,7 @@ export const costomConfirm = {
             });
         });
 
-        document.addEventListener('keyup', (e) => {
+        confirmEl.addEventListener('keyup', (e) => {
             if (e.key === "Escape") {
                 this.close(confirmEl);
             }
